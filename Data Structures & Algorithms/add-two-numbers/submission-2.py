42e72d2df2d438576059e294a6721f749b7f6d2f
DanielT <66112049+danieltsanghang@users.noreply.github.com>
2026-04-11 23:04:28 +0800

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        # l1 : [9,0,9]
+        # l2 : [9,9,9]
+        # re :N,8,1]
+
+        # l1 : [9,0,9]
+        # l2 : [9,9,9]
+        # re : [8,0,1]
+
+        # l1 : [9,0,9]
+        # l2 : [9,9,9]
+        # re : [8,0,9,1]
+        cur = head
+
+        while l1 or l2:
+            l1Val = 0 if l1 is None else l1.val
+            l2Val = 0 if l2 is None else l2.val
+            result = l1Val + l2Val + cur.val
+            if cur.next:
+                result += (cur.next.val * 10)
+            
+            remainder = result % 10
+            print("val for", l1Val, l2Val, 'is:', result, remainder)
+            cur.val = remainder
+            if result >= 10:
+                cur.next = ListNode((result-remainder)//10)
+            # elif l1.next is not None or l2.next is not None: cur.next = ListNode()
+
+            l1 = l1 if l1 is None else l1.next
+            l2 = l2 if l2 is None else l2.next
+            if (l1 or l2) and not cur.next:
+                cur.next = ListNode()
+            cur = cur.next
+        return head
+            
+        
